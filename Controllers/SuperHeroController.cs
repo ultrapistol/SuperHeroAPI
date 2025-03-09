991eae06902eb6e7a4821a26ad8e6c3b41937d8d
Matheus Brito <ultrapistol@gmail.com>
2025-03-09 17:24:51 -0300

    Add project files.

---
+++ Controllers/SuperHeroController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPI_Dotnet8.Entities;
+
+namespace SuperHeroAPI_Dotnet8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
+        {
+            var heroes = new List<SuperHero>
+            {
+                new SuperHero
+                {
+                    Id = 1,
+                    Name = "Spiderman",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York City"
+                }
+            }; 
+
+            return Ok(heroes);
+        }
+    }
+}
---
+++ Entities/SuperHero.cs
@@ -0,0 +1,14 @@
+﻿namespace SuperHeroAPI_Dotnet8.Entities
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+
+
+    }
+}
