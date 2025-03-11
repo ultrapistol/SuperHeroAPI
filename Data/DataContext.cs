95cdba5471965ff2bdd9bfcee218d85d0d2fb935
Matheus Brito <ultrapistol@gmail.com>
2025-03-10 23:58:31 -0300

    Continuação API

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI_Dotnet8.Entities;
+
+namespace SuperHeroAPI_Dotnet8.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
