fe3394e97f9d4705d77d3b9b6f00e96b3afa4e94
Matheus Brito <ultrapistol@gmail.com>
2025-03-18 22:22:43 -0300

    finalização de CRUD

--- Controllers/SuperHeroController.cs
+++ Controllers/SuperHeroController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Components.Web.Virtualization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI_Dotnet8.Data;
 using SuperHeroAPI_Dotnet8.Entities;
 
 namespace SuperHeroAPI_Dotnet8.Controllers
@@ -9,22 +11,42 @@ namespace SuperHeroAPI_Dotnet8.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
+
+        private readonly DataContext _context;
+
+        public SuperHeroController(DataContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
         {
-            var heroes = new List<SuperHero>
-            {
-                new SuperHero
-                {
-                    Id = 1,
-                    Name = "Spiderman",
-                    FirstName = "Peter",
-                    LastName = "Parker",
-                    Place = "New York City"
-                }
-            }; 
+            var heroes = await _context.SuperHeroes.ToListAsync();
+
 
             return Ok(heroes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> GetHero(int id)
+        {
+            var hero = await _context.SuperHeroes.FindAsync(id);
+            if (hero is null)
+                return NotFound("Hero not found.");
+
+
+            return Ok(hero);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero([FromBody]SuperHero hero)
+        {
+             _context.SuperHeroes.Add(hero);
+             await _context.SaveChangesAsync();
+
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
         }
     }
 }
